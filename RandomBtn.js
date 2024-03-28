f96d36af39c60876e68610bc191286ec7c3aaf58
Arefevpro <arefevmsk@gmail.com>
2024-03-28 18:39:50 +0300

    ./

--- RandomBtn.js
+++ RandomBtn.js
@@ -1,8 +1,10 @@
 function generateRandomNumber() {
   // Генерируем случайное число от 1 до 10000
   // var randomNumber = Math.floor(Math.random() * 10000); 
-  var randomNumber = (Math.random() * 10 );
-  
+  var objects = ["Яблоко", "Груша", "Банан", "Апельсин", "Ананас", "Мандарин"];
+  var object = objects[Math.floor(Math.random() * objects.length)];
+  // var randomNumber = (Math.random() * 10 );
+  // var randomNumberC = Math.round(randomNumber)
   // Выводим случайное число на страницу
-  document.getElementById("randomNumber").innerHTML = "Случайное число: " + randomNumber;
+  document.getElementById("randomNumber").innerHTML = "Твой любимый фрук: " + object;
 }
