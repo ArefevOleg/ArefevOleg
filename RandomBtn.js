67ed635fcc6214351f687e240b6e25a14aba57d0
Arefevpro <arefevmsk@gmail.com>
2024-03-28 17:22:55 +0300

    ./

--- RandomBtn.js
+++ RandomBtn.js
@@ -1,5 +1,5 @@
 function generateRandomNumber() {
-  // Генерируем случайное число от 1 до 100
+  // Генерируем случайное число от 1 до 10000
   var randomNumber = Math.floor(Math.random() * 10000) + 1;
   
   // Выводим случайное число на страницу
