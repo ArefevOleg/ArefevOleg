1f94f185924999256a38c010e055a5a4bd6de1c4
Arefevpro <arefevmsk@gmail.com>
2024-03-28 17:16:44 +0300

    ./

--- ScriptJS.js
+++ ScriptJS.js
@@ -1,1 +1,7 @@
-console.log(Math.random())
+function generateRandomNumber() {
+  // Генерируем случайное число от 1 до 100
+  var randomNumber = Math.floor(Math.random() * 10000) + 1;
+  
+  // Выводим случайное число на страницу
+  document.getElementById("randomNumber").innerHTML = "Случайное число: " + randomNumber;
+}
