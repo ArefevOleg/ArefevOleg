d2c5e5ac6f6d8e78f06e32becd20918be6e77094
Arefevpro <arefevmsk@gmail.com>
2024-03-28 23:12:21 +0300

    ./

--- RandomBtn.js
+++ RandomBtn.js
@@ -8,3 +8,14 @@ function generateRandomNumber() {
   // Выводим случайное число на страницу
   document.getElementById("randomNumber").innerHTML = "Твой любимый фрук: " + object;
 }
+
+
+const saveEmail = () => {
+  // В реальности email приходит из формы
+  const email = '  SuppORT@hexlet.IO';
+  // обрезаем пробельные символы
+  const trimmedEmail = email.trim();
+  const preparedEmail = trimmedEmail.toLowerCase();
+  console.log(preparedEmail);
+  // здесь будет запись в базу данных
+};
